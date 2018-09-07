25af512c7e46ba4c621f87a1525724db31b86d48
imendez77 <joshepvdez77@gmail.com>
2018-09-06 21:28:36 -0600

    versión 1.0.2

--- ProyectoDAUTE/src/com/dao/DAOUsuario.java
+++ ProyectoDAUTE/src/com/dao/DAOUsuario.java
@@ -1,9 +1,97 @@
 package com.dao;
 
+import com.modelo.Operaciones;
+import com.conexion.Conexion;
+import com.modelo.Usuario;
+import java.util.ArrayList;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Isra Méndez
  */
-public class DAOUsuario {
+public class DAOUsuario implements Operaciones{
+    Conexion db = new Conexion();
+    Usuario user = new Usuario();
+    
+    @Override
+    public boolean insertar(Object obj) {
+        user = (Usuario) obj;
+        Connection con;
+        PreparedStatement pst;
+        String sql = "insert into usuario values (0, ?, ?, ?, 1, ?, now(), 0)";
+        
+        try 
+        {
+            Class.forName(db.getDriver());
+            con = DriverManager.getConnection(db.getUrl(), db.getUser(), db.getPassword());
+            pst = con.prepareStatement(sql);
+            
+            pst.setString(2, user.getEmail());
+            pst.setString(3, user.getUsername());
+            pst.setString(4, user.getPassword());
+            pst.setInt(5, user.getId_rol());
+            
+            int row = pst.executeUpdate();
+            
+            if(row>0)
+            {
+                con.close();
+                return true;
+            }else{
+                con.close();
+                return false;
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al insertar: "+e.getMessage(), "ERROR", 0);
+            return false;
+        }
+    }
+
+    @Override
+    public boolean modificar(Object obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean eliminar(Object obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Object[]> mostrar() {
+        ArrayList<Object[]> datos = new ArrayList<>();
+        Connection con;
+        PreparedStatement pst;
+        ResultSet rs;
+        String sql = "select * from usuario";
+        
+        try 
+        {
+            Class.forName(db.getDriver());
+            con = DriverManager.getConnection(db.getUrl(), db.getUser(), db.getPassword());
+            pst = con.prepareStatement(sql);
+            rs = pst.executeQuery();
+            
+            while(rs.next())
+            {
+                Object[] rows = new Object[8];
+                for (int i = 0; i < 8; i++) {
+                    rows[i] = rs.getObject(i+1);
+                }
+                datos.add(rows);
+            }
+            con.close();
+        } 
+        catch (Exception e) 
+        {
+            JOptionPane.showMessageDialog(null, "Error al Mostrar: "+e.getMessage(), "ERROR", 0);
+        }
+        finally
+        {
+            return datos;
+        }
+    }
     
 }
--- ProyectoDAUTE/src/com/modelo/Usuario.java
+++ ProyectoDAUTE/src/com/modelo/Usuario.java
@@ -5,5 +5,52 @@ package com.modelo;
  * @author Isra Méndez
  */
 public class Usuario {
+    private String username;
+    private String password;
+    private String email;
+    private int id_rol;
+
+    public Usuario() {
+    }
+
+    public Usuario(String username, String password, String email, int id_rol) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.id_rol = id_rol;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId_rol() {
+        return id_rol;
+    }
+
+    public void setId_rol(int id_rol) {
+        this.id_rol = id_rol;
+    }
+    
     
 }
